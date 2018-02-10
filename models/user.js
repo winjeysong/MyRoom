6e02300e486255d2330a8381af49772c55be5501
winjeysong <winjey-song@163.com>
2018-02-10 11:58:18 +0800

    add mongoose model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+/**
+ * data model
+ */
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cellphone: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
