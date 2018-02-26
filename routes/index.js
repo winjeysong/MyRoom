cdb4f29bd3561ec75db3abe98e9d6181e0ea63b4
winjeysong <winjey-song@163.com>
2018-02-26 21:01:07 +0800

    use browserHistory & fix issue
    issue details: comments in routes/index

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,22 @@
 
 const router = require('koa-router')();
 const api = require('./api/index');
+const fs = require('fs');
+const path = require('path');
+const { build_filename, build_path } = require('../config/config');
 
-router.use('/api', api.routes(), api.allowedMethods());
+const filePath = `${path.join(build_path, build_filename)}.html`;
+
+router
+  // fix the issue:
+  // front-end router with browserHistory
+  // forward to the right page with Link (react-router)
+  // it will be 404NotFound after refresh the page
+  .get('/*', async (ctx) => {
+    const html = fs.readFileSync(path.resolve(filePath));
+    ctx.type = 'html';
+    ctx.body = html;
+  })
+  .use('/api', api.routes(), api.allowedMethods());
 
 module.exports = router;
