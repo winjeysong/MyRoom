f5b61908cc9ab253927045e525a1a622e2e6275f
winjeysong <winjey-song@163.com>
2018-02-28 00:54:20 +0800

    add theme file

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+module.exports = () => {
+  return {
+    // --------- Color
+    '@primary-color': '#4caf50',
+
+    // // --------- Link
+    // '@link-active-color': '#fe6900',
+    // '@link-hover-color': '#fe6900',
+
+    // // -------- Form
+    // '@label-color': '#666666',
+
+    // // -------- Table
+    // '@table-header-bg': '#e0f0ff',
+    // '@table-row-hover-bg': '#f3f4f5',
+  };
+};
