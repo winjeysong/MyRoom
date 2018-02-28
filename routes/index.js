68bf05dcea0c66af525539862a8c4c84725e9f25
winjeysong <winjey-song@163.com>
2018-02-28 13:31:09 +0800

    use async readFile

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,15 @@ const path = require('path');
 const { build_filename, build_path } = require('../config/config');
 
 const filePath = `${path.join(build_path, build_filename)}.html`;
+const readFile = new Promise((resolve, reject) => {
+  fs.readFile(path.resolve(filePath), (err, data) => {
+    if (!err) {
+      resolve(data);
+    } else {
+      reject(err);
+    }
+  });
+});
 
 router
   // fix the issue:
@@ -16,7 +25,7 @@ router
   // forward to the right page with Link (react-router)
   // it will be 404NotFound after refresh the page
   .get('/*', async (ctx) => {
-    const html = fs.readFileSync(path.resolve(filePath));
+    const html = await readFile;
     ctx.type = 'html';
     ctx.body = html;
   })
