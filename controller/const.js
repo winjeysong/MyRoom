c94b82b48af797841dd1f84e2b594a1d69b719ec
winjeysong <winjey-song@163.com>
2018-03-01 01:47:27 +0800

    modify resultmsg constants

--- controller/const.js
+++ controller/const.js
@@ -7,8 +7,8 @@ module.exports = {
     REGISTER_FAILURE: '注册未成功，请重新注册',
     REGISTER_USER_EXISTED: '用户名已存在',
     REGISTER_SUCCESS: '注册成功',
-    LOGIN_USER_NOT_EXISTENCE: '用户不存在',
+    LOGIN_USER_NOT_EXISTENCE: '用户不存在，请注册',
     LOGIN_SUCCESS: '登陆成功',
-    LOGIN_PASSWD_ERR: '密码错误',
+    LOGIN_PASSWD_ERR: '密码错误，请重新输入',
   },
 };
