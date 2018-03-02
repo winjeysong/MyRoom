66e73f688a1e947034dcf11ccd4cb38cba396f19
winjeysong <winjey-song@163.com>
2018-03-02 21:26:48 +0800

    modify

--- controller/display.js
+++ controller/display.js
@@ -6,15 +6,10 @@ const User = require('../models/user');
 
 async function userDisplay(ctx) {
   const id = ctx.params.id;
-  const select = 'username password email cellphone residence website';
+  const select = 'username email cellphone residence website';
   await User.findById(id, select, (err, user) => {
-    if (err) {
-      throw err;
-    }
     if (user) {
       if (id === String(user._id)) {
-        ctx.type = 'json';
-        user.password = '******';
         ctx.body = user;
       }
     }
