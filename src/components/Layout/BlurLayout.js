2e705f4b4dfdd04d12be4336090bf5d437319339
winjeysong <winjey-song@163.com>
2018-03-12 16:08:04 +0800

    add link

--- src/components/Layout/BlurLayout.js
+++ src/components/Layout/BlurLayout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { Link } from 'dva/router';
 import styles from './BlurLayout.css';
 
 const { Header, Content } = Layout;
@@ -11,7 +12,7 @@ function BlurLayoutWrapper({ content }) {
       <Layout className={styles.layout}>
         <Header className={styles.header}>
           <div className="logo" />
-          Welcome to My Room.
+          <Link to="/" style={{ color: '#eaeaea' }}>Welcome to My Room.</Link>
         </Header>
         <Content style={{ height: '100%', padding: '35px 30px' }}>
           <div className={styles.content}>{content}</div>
