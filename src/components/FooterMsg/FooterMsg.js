6729c6c6dba460d0e104cca0f7492a18a6fec9d4
winjeysong <winjey-song@163.com>
2018-03-12 15:55:21 +0800

    modify footer

--- src/components/FooterMsg/FooterMsg.js
+++ src/components/FooterMsg/FooterMsg.js
@@ -6,12 +6,12 @@ function FooterMsg() {
   return (
     <div className={styles.normal}>
       <div className={styles['footer-wrapper']}>
-        Developed with <br />
+        <a href="https://reactjs.org/" className={styles['footer-link']}><Icon className={styles['footer-icon']} type="heart" /> React</a><br />
         <a href="http://2x.ant.design/index-cn" className={styles['footer-link']}><Icon className={styles['footer-icon']} type="heart" /> AntD</a><br />
         <a href="https://github.com/dvajs/dva" className={styles['footer-link']}><Icon className={styles['footer-icon']} type="heart" /> Dva</a><br />
         <a href="http://koajs.com/" className={styles['footer-link']}><Icon className={styles['footer-icon']} type="heart" /> Koa2</a><br />
         <a href="http://mongoosejs.com/" className={styles['footer-link']}><Icon className={styles['footer-icon']} type="heart" /> Mongoose</a>
-        <br />@winjeysong
+        <br /><a href="http://winjeysong.com" className={styles['footer-link']}>@winjeysong</a>
       </div>
     </div>
   );
